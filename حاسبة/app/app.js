0f09e7c0bdfaa101b5f4d44136dc7dbfaea0e78b
ahmed mo-Eid <89750867+Ahmedmeid99@users.noreply.github.com>
2022-01-08 20:49:39 +0200

    Update app.js

--- حاسبة/app/app.js
+++ حاسبة/app/app.js
@@ -2,36 +2,63 @@
 //select Element
 const nums = document.querySelectorAll('.row span');
 const input = document.querySelector('input')
-console.log(input)
 nums.forEach((num) => {
     num.addEventListener('click', () => {
         if (num.textContent != "del") {
-            const current = "";
-            input.value += num.textContent + current;
+            input.value += num.textContent;
         }
     })
 })
 const del = document.querySelector('.del');
 del.addEventListener('click', () => {
-    input.value = input.value.slice(0, input.value.length - 1)
+    //remove item by item 
+    // input.value = input.value.slice(0, input.value.length - 1)
+    //Remove all items
+    input.value = " ";
 })
 // (=) onclick 
 const solve = document.querySelector('.solve')
 solve.addEventListener('click', () => {
     //If the first item is + or - or * or /
-    if (input.value[0] === "+") { alert("Enter number first") }
-    else if (input.value[0] === "-") { alert("Enter your numbers first") }
-    else if (input.value[0] === "*") { alert("Enter your numbers first") }
-    else if (input.value[0] === "/") { alert("Enter your numbers first") }
+    if (input.value[0] === "+" || input.value[0] === "-" || input.value[0] === "*" || input.value[0] === "/") { alert("Enter number first") }
     //If the last item is + or - or * or /
-    else if (input.value[input.value.length - 1] === "+") { alert("Enter number last") }
-    else if (input.value[input.value.length - 1] === "-") { alert("Enter number last") }
-    else if (input.value[input.value.length - 1] === "*") { alert("Enter number last") }
-    else if (input.value[input.value.length - 1] === "/") { alert("Enter number last") }
+    else if (input.value[input.value.length - 1] === "+" || input.value[input.value.length - 1] === "-" || input.value[input.value.length - 1] === "*" || input.value[input.value.length - 1] === "/") { alert("Enter number last") }
     else {
-        if (input.value.includes("+")) { input.value = +input.value.substring(0, input.value.indexOf("+")) + +input.value.substring(input.value.indexOf("+") + 1) }
-        else if (input.value.includes("-")) { input.value = +input.value.substring(0, input.value.indexOf("-")) - +input.value.substring(input.value.indexOf("-") + 1) }
-        else if (input.value.includes("*")) { input.value = +input.value.substring(0, input.value.indexOf("*")) * +input.value.substring(input.value.indexOf("*") + 1) }
-        else if (input.value.includes("/")) { input.value = +input.value.substring(0, input.value.indexOf("/")) / +input.value.substring(input.value.indexOf("/") + 1) }
+        solving()
+        // console.log(typeof (input.value))
     }
 })
+function solving() {
+    if (input.value.includes("+")) {
+        const result = input.value.split("").filter(function (ele) {
+            return !isNaN(parseInt(ele));
+        }).reduce(function (acc, current) {
+            return +acc + +current;
+        })
+        input.value = result
+    }
+    else if (input.value.includes("-")) {
+        const result = input.value.split("").filter(function (ele) {
+            return !isNaN(parseInt(ele));
+        }).reduce(function (acc, current) {
+            return +acc - +current;
+        })
+        input.value = result
+    }
+    else if (input.value.includes("*")) {
+        const result = input.value.split("").filter(function (ele) {
+            return !isNaN(parseInt(ele));
+        }).reduce(function (acc, current) {
+            return +acc * +current;
+        })
+        input.value = result
+    }
+    else if (input.value.includes("/")) {
+        const result = input.value.split("").filter(function (ele) {
+            return !isNaN(parseInt(ele));
+        }).reduce(function (acc, current) {
+            return +acc / +current;
+        })
+        input.value = result
+    }
+}
